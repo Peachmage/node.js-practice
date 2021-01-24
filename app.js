72fa72b116a285978474baaa2eb64676bc86c6a4
Peachmage <danissh@mail.ru>
2021-01-24 21:54:34 +0300

    Tutorial done!

--- app.js
+++ app.js
@@ -1,17 +1,55 @@
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
 const mongoose = require('mongoose');
+const flash = require('connect-flash');
+const session = require('express-session');
+const passport = require('passport');
 
 const app = express();
 
+//passport config
 
+require('./config/passport')(passport);
+
+//DB Config
+
+const db = require('./config/keys').mongoURI;
+
+mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('MongoDB connected.'))
+    .catch(err => console.log(err));
 
 //EJS
 app.use(expressLayouts);
 app.set('view engine', 'ejs');
 
+//bodyparser
+app.use(express.urlencoded({ extended: false})); 
 
 
+//Express session middleware
+
+app.use(session({
+    secret: 'secret very too secret',
+    resave: false,
+    saveUninitialized: true,
+  }))
+
+  //passport middleware 
+app.use(passport.initialize());
+app.use(passport.session());
+
+  
+app.use(flash());
+
+//global vars
+
+app.use((req, res, next) => {
+    res.locals.success_msg = req.flash('success_msg');
+    res.locals.error_msg = req.flash('error_msg');
+    res.locals.error = req.flash('error');
+    next();
+})
 
 //routes
 
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../config/auth');
 
 router.get('/', (req, res) => res.render('welcome'));
+
+
+router.get('/dashboard', auth.ensureAuthenticated, (req, res) => 
+res.render('dashboard', {
+    name: req.user.name
+}));
 
 module.exports = router;
